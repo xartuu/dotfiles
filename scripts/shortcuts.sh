79ac471771a878e5908b28d2802638eb62c17734
Artur Kociszewski <me@artuu.pl>
2021-12-24 06:06:53 +0100

    Added `.gitconfig` shortcut from Windows

--- scripts/shortcuts.sh
+++ scripts/shortcuts.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Configs
+ln -snf "/mnt/c/Users/artuu/.gitconfig" "$HOME/.gitconfig"
+
+# Programs
 mkdir -p "$HOME/bin"
 ln -snf "$(command -v plink.exe)" "$HOME/bin/ssh"
 ln -snf "$(command -v pscp.exe)" "$HOME/bin/scp"
