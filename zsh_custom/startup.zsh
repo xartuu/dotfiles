e2bf9c5b4bd33ec751aae9a45f0e66c46b4b1aa4
Artur Kociszewski <me@artuu.pl>
2022-01-17 03:42:22 +0100

    Added auto startup of `MySQL` and `Valet`

--- zsh_custom/startup.zsh
+++ zsh_custom/startup.zsh
@@ -1,2 +1,6 @@
 # Start Laravel Valet
-valet start
+service nginx status &> /dev/null  || valet start > /dev/null 2>&1
+
+# Start MySQL
+service mysql status &> /dev/null || sudo service mysql start > /dev/null 2>&1
+# takeout enable mysql
