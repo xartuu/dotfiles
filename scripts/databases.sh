8fb66cf7bb542df3668fc088cd85be15fd0e9d5c
Artur Kociszewski <me@artuu.pl>
2022-01-17 03:41:00 +0100

    Added `MySQL` database installation

--- scripts/databases.sh
+++ scripts/databases.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 # Installs MySQL
-sudo apt-get install -y mysql-client
+sudo apt-get install -y mysql-client mysql-server
+mysql --version
 
-# takeout enable mysql
+sudo usermod -d /var/lib/mysql/ mysql
+sudo service mysql start
+sudo  mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY ''; FLUSH PRIVILEGES;"
+sudo service mysql restart
